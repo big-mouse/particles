df7a336416bc1984253c02a691f029345ada0255
Tsur <prucheta@gmail.com>
2016-07-29 22:09:05 +0200

    bumped to 0.0.2 :mouse:

--- src/particles.js
+++ src/particles.js
@@ -4,6 +4,9 @@
  * @returns Function(Object, {num, size, radius, color, speed, distortion})
  */
 // Note: not using "export default" es6 to bypass .default when using library globally from window scope.
+
+import _ from 'lodash';
+
 module.exports = exports = function(options= {radius:30}){
 
     let iteration = 1;
@@ -34,8 +37,8 @@ module.exports = exports = function(options= {radius:30}){
 
                     if(particle.radius) crad =  particle.radius * Math.sin(iteration / 30);
 
-                    const x = self.mouseCoords.x + (crad + particle.distortion * Math.sin(iteration / particle.tmod)) * Math.cos(particle.angle * 180 / Math.PI);
-                    const y = self.mouseCoords.y + (crad + particle.distortion * Math.sin(iteration / particle.tmod)) * Math.sin(particle.angle * 180 / Math.PI);
+                    const x = self.mouseCoords.x + window.pageXOffset + (crad + particle.distortion * Math.sin(iteration / particle.tmod)) * Math.cos(particle.angle * 180 / Math.PI);
+                    const y = self.mouseCoords.y + window.pageYOffset + (crad + particle.distortion * Math.sin(iteration / particle.tmod)) * Math.sin(particle.angle * 180 / Math.PI);
 
                     self.canvas.ctx.moveTo(particle.lastPos.x, particle.lastPos.y);
                     self.canvas.ctx.lineTo(x, y);
@@ -70,7 +73,7 @@ function getParticles(options, mouse){
         const angle = rand(0, 359);
 
         particles = [...particles, {
-            color: options.color ? options.color : `hsla(${(rand(0,1) > .5) ? ~~rand(0, 20) : ~~rand(180, 200)},100%,60%,1)`,
+            color: options.color ? (_.isFunction(options.color) ? options.color() : options.color) : `hsla(${(rand(0,1) > .5) ? ~~rand(0, 20) : ~~rand(180, 200)},100%,60%,1)`,
             distortion: rand(0,options.distortion ? options.distortion : 10),
             tmod: rand(5, 15),
             size: rand(1, options.size),
